6b3c342cd2e0bb85c1203acd82a3aa84ca74952a
RebeccaGS <68403893+RebeccaGS@users.noreply.github.com>
2023-09-27 01:04:39 -0300

    quest 11 e 12 feitas

--- trab1.py
+++ trab1.py
@@ -1,22 +1,44 @@
+
+ # add tratamento de erros em primeira eq
+
+# Questão 11
 ''' Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
 o produto do dobro do primeiro com metade do segundo . 2A vezes B/2
 a soma do triplo do primeiro com o terceiro.
 o terceiro elevado ao cubo.
 '''
-
-
-def pedirNumero():
+def operacoesBasicas():
+    # coletando e convertendo os dados
+    primeiroInt = input("Coloque um numero inteiro: ")
+    primeiroInt = int(primeiroInt)
     
-    coletarPrimeiroInt = input("Coloque um numero inteiro: ")
-    primeiroInt = int(coletarPrimeiroInt)
-    coletarSegundoInt = input("Coloque outro numero inteiro: ")
-    segundoInt = int(coletarSegundoInt)
+    segundoInt = input("Coloque outro numero inteiro: ")
+    segundoInt = int(segundoInt)
+    
     numReal = input("Coloque um numero real: ")
+    numReal = float(numReal)
 
-    print(type(primeiroInt))
-    produtoDobro1Metade2 = primeiroInt*2*segundoInt%2
-    print("produto do dobro do primeiro com metade do segundo: ")
-    print(produtoDobro1Metade2)
+    # equacoes
+    produtoDobro1Metade2 = primeiroInt*2*segundoInt/2
+    somaTriplo12 = primeiroInt*3 + numReal
+    terceiroCubo = numReal*numReal*numReal
+
+    # prints
+    print("Produto do dobro do primeiro com metade do segundo: " + str(produtoDobro1Metade2))
+    print("A soma do triplo do primeiro com o terceiro: " + str(somaTriplo12))
+    print("O terceiro elevado ao cubo: " + str(terceiroCubo))
 
 
-pedirNumero()
+# Questão 12
+# Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+def pesoIdeal():
+    altura = input("digite sua altura: ")
+    altura = float(altura)
+    pesoIdeal = (72.7*altura) - 58
+    print("O peso ideal é: " + str(pesoIdeal))
+
+
+# Questão 13
+# Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+# Para homens: (72.7*h) - 58
+# Para mulheres: (62.1*h) - 44.7
